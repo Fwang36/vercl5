16994ab1dd5968036fe1fbccf96ad68045b29d86
Francis Wang <franciswang@NJKNHVVGV9.local>
2023-02-15 16:30:28 -0500

    comment out

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,8 +1,13 @@
 import { renderToString } from "react-dom/server";
 import { RemixServer } from "remix";
 import type { EntryContext } from "remix";
-import * as Sentry from "@sentry/remix";
+// import * as Sentry from "@sentry/remix";
 
+// Sentry.init({
+//   dsn: "https://f5ea3254102d4890aff3291dce1e1478:63cec28b01e34b5b96227602629fc651@o1407376.ingest.sentry.io/4504606337007616",
+//   tracesSampleRate: 1,
+//   debug:true
+// });
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
@@ -13,11 +18,6 @@ export default function handleRequest(
     <RemixServer context={remixContext} url={request.url} />
   );
 
-  Sentry.init({
-    dsn: "",
-    tracesSampleRate: 1,
-    debug:true
-  });
 
   responseHeaders.set("Content-Type", "text/html");
 
